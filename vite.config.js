ed74f6d91cca897350574717e9d009c1b76b98f7
みょん <topia.myon@gmail.com>
2022-09-26 19:12:34 +0900

    fix the base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import { million } from 'million/vite-plugin-million';
+import { defineConfig } from "vite";
+import { million } from "million/vite-plugin-million";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [million({ react: true })],
 });
